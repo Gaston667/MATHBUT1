8b8703c5f3a215cf9a63917920b7fd154097e0f1
Diallo Algassimou Pellel <algassimoupelleldiallo2005@gmail.com>
2025-02-12 17:31:32 +0100

    parcours TP3 a finir

---
+++ Graphes/TP3/Graphe.java
@@ -0,0 +1,195 @@
+import java.util.*;
+/**
+ * Classe definissant des graphes au sens mathematiques
+ * @author Luc Dartois
+ * @version 1.0
+*/
+
+public class Graphe{
+/**
+ * Le nombre de sommets du graphe, numerotes de 0 a ordre-1
+ */
+private int ordre;
+/**
+ * Ses aretes donnees sous forme de matrice carre d'adjacence (voir classe MatriceCarre)
+ */
+private MatriceCarre adj;
+/**
+ * Indique si le graphe est oriente ou non
+ */
+private boolean oriente;
+
+/**
+ * Construit un graphe vide
+ *@param ord indique l'ordre du graphe
+ *@param o indique s'il est oriente
+ */
+public Graphe(int ord,boolean o){
+	this.ordre=ord;
+	this.adj=new MatriceCarre(ord);
+	this.oriente=o;
+}
+/**
+ * Getter pour l'ordre
+ *@return L'ordre du graphe
+ */
+public int getOrdre(){
+	return this.ordre;
+}
+/**
+ * Renvoit vrai s'il y a une arete de i a j
+ *@param i,j Deux sommets du graphe
+ *@return Vrai si le graphe possede une arete de i a j
+ */
+public boolean getArete(int i,int j){
+	return this.adj.getCoeff(i,j)==1;
+}
+/**
+ * Ajoute une arete de i a j
+ *@param i,j Deux sommets du graphe
+ */
+public void ajoutArete(int i,int j){
+	if(i>=this.ordre||j>=this.ordre||i<0||j<0){
+		throw new IllegalArgumentException("Erreur : Sommet inexistant.");
+	}
+	this.adj.setCoeff(i,j,1);
+	if(!this.oriente){
+	this.adj.setCoeff(j,i,1);
+	}
+}
+/**
+ * Affiche la liste des voisins de i
+ *@param i Un sommet du graphe
+ */
+public void voisinage(int i){
+	if(!this.oriente){
+		System.out.print("Voisins de "+i+" : ");
+		for(int j=0;j<this.ordre;j++){
+		if(this.adj.getCoeff(i,j)==1){
+			System.out.print(j+", ");
+		}
+		}
+	System.out.println();
+	}
+	else{
+		System.out.print("Voisins sortants de "+i+" : ");
+		for(int j=0;j<this.ordre;j++){
+		if(this.adj.getCoeff(i,j)==1){
+			System.out.print(j+", ");
+		}
+		}
+	System.out.println();
+			System.out.print("Voisins entrants de "+i+" : ");
+		for(int j=0;j<this.ordre;j++){
+		if(this.adj.getCoeff(j,i)==1){
+			System.out.print(j+", ");
+		}
+		}
+	System.out.println();
+
+	}
+}
+/**
+ * Calcule la somme du nombre de voisins de tous les sommets
+ *@return Un entier representant la somme de tous les voisins.
+ */
+public int sommeVoisins(){
+int s=0;
+	for(int i=0;i<this.ordre;i++){
+		for(int j=0;j<this.ordre;j++){
+			s+=this.adj.getCoeff(i,j);
+		}
+	}
+	return s;
+}
+
+public String toString(){
+	String s="Graphe ";
+	if(!this.oriente){
+		s+="non ";
+	}
+	s+="oriente de taille : "+this.ordre+".\n";
+
+	return s;
+}
+
+
+public boolean equals(Graphe g){
+	if(this.ordre!=g.ordre)
+		return false;
+	if(this.oriente!=g.oriente)
+		return false;
+	if(!this.adj.equals(g.adj))
+		return false;
+	return true;
+}
+
+// Fonction qui permet de calculer la matrice contenant les chemins d'une longeur donnee 
+
+public MatriceCarre nombreDeChemins(int longeur ){
+	MatriceCarre res = this.adj;
+	for (int i = 0; i < longeur-1; i++) {
+		res = adj.multiplication(res);
+	}
+	return res;
+}
+
+
+
+/*public boolean estConnexe(){
+	boolean res = false; 
+	for (int i = 1; i <= this.ordre; i++) {
+		
+	}
+}*/
+
+public LinkedList<Integer> getVoisins(int i){
+	LinkedList<Integer> res =new LinkedList<Integer>();
+
+	for (int l=0;l<this.ordre; l++) {
+		if (this.adj.getCoeff(i,l) == 1) {
+			res.offer(l);
+		}
+	}
+	return res;
+}
+//Fonction effectuant le parcours en largeur d'un graphe g a partir d'un sommet v.
+public void parcoursLargeur(int v){
+	LinkedList<Integer> file =new LinkedList<Integer>();
+	LinkedList<Integer> voisins =new LinkedList<Integer>();
+	file.offer(v);
+	int i = 1;
+	int x;
+
+	// Gesttion de la sortie(alpha,dist) et on les initialise
+	int[][] alpha =new int[2][this.ordre];
+	for (int ligne=0;ligne<this.ordre;ligne++ ) {
+		for (int col=0; col<2;col++) {
+			alpha[ligne][col]=0;
+		}
+	}
+
+	while(!file.isEmpty()){
+		x = file.remove();
+		voisins = this.getVoisins(x);
+		for (int voisin : voisins) {
+			if (alpha[0][voisin]==0){
+				i+=1;
+				alpha[0][voisin] = i;
+				alpha[1][voisin] = alpha[1][x]+1;
+				file.offer(voisin);
+			}
+		}
+	}
+
+	for (int ligne=0;ligne<this.ordre;ligne++ ) {
+		for (int col=0; col<2;col++) {
+			System.out.println(alpha[ligne][col]);
+		}
+		System.out.println("");
+	}
+
+
+
+}
+}
---
+++ Graphes/TP3/MatriceCarre.java
@@ -0,0 +1,111 @@
+/**
+ * Une classe definissant une matrice carree
+ * @author Luc Dartois
+ * @version 1.0
+ */
+public class MatriceCarre{
+	private int n; //taille de la matrice
+	private int[][] m; //La matrice
+
+/**
+ * Constructeur creant une matrice vide
+ *@param t La taille de la matrice
+ */
+	public MatriceCarre(int t){
+		this.n=t;
+		this.m=new int[t][t];
+	}
+/**
+ * Constructeur copiant une matrice donnee dans une nouvelle matrice
+ *@param a La matrice a copier
+ */	
+	public MatriceCarre(MatriceCarre a){
+		this.n=a.n;
+		this.m=new int[this.n][this.n];
+		for(int i=0;i<this.n;i++){
+			for(int j=0;j<this.n;j++){
+				this.m[i][j]=a.m[i][j];
+			}
+		}
+	}	
+/**
+ * Methode permettant de definir le coefficient d'une matrice
+ *@param i Le coefficient de ligne
+ *@param j Le coefficient de colonne
+ *@param val La valeur du coefficient
+ */
+	public void setCoeff(int i,int j,int val){
+	if(i>=this.n||j>=this.n||i<0||j<0){
+		throw new IllegalArgumentException("Erreur : Hors limites de la matrice.");
+	}
+		this.m[i][j]=val;
+	}
+/**
+ * Methode permettant de recuperer le coefficient d'une matrice
+ *@param i Le coefficient de ligne
+ *@param j Le coefficient de colonne
+ *@return Le coefficient
+ */
+	public int getCoeff(int i,int j){
+	if(i>=this.n||j>=this.n||i<0||j<0){
+		throw new IllegalArgumentException("Erreur : Hors limites de la matrice.");
+	}
+	return this.m[i][j];		
+	}
+
+	public String toString(){
+		String 	s="  ";
+	for(int j=0;j<this.n;j++){
+			s+="| "+j;
+	}
+	s+="|\n";
+	for(int i=0;i<this.n;i++){
+		s+=i+" | ";
+		for(int j=0;j<this.n;j++){
+			s+=this.m[i][j]+"| ";
+		}
+		s+="\n";
+	}
+	return s;
+	}
+
+	public boolean equals(MatriceCarre a){
+		if(this.n!=a.n)
+			return false;
+
+		for(int i=0;i<this.n;i++){
+			for(int j=0;j<this.n;j++){
+				if(this.m[i][j]!=a.m[i][j])
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public MatriceCarre multiplication(MatriceCarre a){
+  		if(this.n!=a.n)
+    		throw new IllegalArgumentException("multiplication de matrices de tailles differentes");
+ 		MatriceCarre res=new MatriceCarre(this.n);
+  		int i,j,k;
+  		for(i=0;i<this.n;i++){
+        	for(j=0;j<this.n;j++){
+          		for(k=0;k<this.n;k++){
+            		res.m[i][j]+=this.m[i][k]*a.m[k][j];
+         		}
+       		}
+   		}
+    return res;
+}
+
+    public MatriceCarre addittion(MatriceCarre a ){
+ 		MatriceCarre res = new MatriceCarre(this.n);
+  		int i,j;
+  		for(i=0;i<this.n;i++){
+        	for(j=0;j<this.n;j++){
+          		res.m[i][j]= this.m[i][j] + a.m[i][j];
+       		}
+   		}
+
+   		return res;
+   	}
+}
